e8d33b2fba4b0eafd1cd2b30c8255c139a503fbd
bryant <eevee@stanford.edu>
2023-08-07 19:56:40 -0600

    Deployment

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "La Morena Arts",
-  description: "Artist. First-Gen. CSU '23",
+  name: "Flair",
+  description: "Expertly designed goods for the workplace, home, and travel.",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
